09d122f1f1a5addf55b05e1d698c55518f153674
Manasi Shilwant <manasishilwant1001@gmail.com>
2026-01-31 17:51:35 +0530

    fix input type error in hello.py

---
+++ hello.py
@@ -0,0 +1,9 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+print("Name :",name)
+print("Age :",age)
+
+age = int(input("Enter your age: "))
+if age > 18:
+    print("You are eligible to vote.")
